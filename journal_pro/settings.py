1dc075da651779d4aa6531fd6d2293c20ae20261
Onah Jonah <onahjonah@gmail.com>
2025-03-27 02:00:52 +0100

    password reset

--- journal_pro/settings.py
+++ journal_pro/settings.py
@@ -129,9 +129,10 @@ LOGIN_URL = 'login'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'in-v3.mailjet.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = 'smpt.gmail.com'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'info@rutonalinkages.ng'
